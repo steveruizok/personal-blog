1e8e514ad979952abb76bfab53af7da5278bffb8
Steve <steveruizok@gmail.com>
2020-10-08 10:23:17 +0100

    Create [slug].tsx

---
+++ pages/api/posts/[slug].tsx
@@ -0,0 +1,22 @@
+import { getPostBySlug } from "../../../lib/api"
+
+export default async (req, res) => {
+  const {
+    query: { slug },
+  } = req
+
+  const post = getPostBySlug("/posts/" + slug)
+
+  if (post) {
+    const d = await fetch("https://localhost:3000/" + post.__resourcePath)
+    console.log(d)
+
+    res.statusCode = 200
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ post }))
+  } else {
+    res.statusCode = 404
+    res.setHeader("Content-Type", "application/json")
+    res.end(JSON.stringify({ post: undefined }))
+  }
+}
