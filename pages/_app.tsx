23f76a809cafb008399283846f59e70a9a2d4f30
Steve Ruiz <steveruizok@gmail.com>
2021-10-21 08:45:20 +0100

    Adds gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import "../styles/globals.css"
+import React from "react"
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
+import useGtag from "~hooks/useGtag"
 import { globalStyles } from "~stitches.config"
-import React from "react"
+import "~styles/globals.css"
 
 function App({ Component, pageProps }: AppProps) {
   globalStyles()
+  useGtag()
 
   return (
     <ThemeProvider>
