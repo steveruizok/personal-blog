4f488f202bddf65b752ee591895959a41116d222
Steve Ruiz <steveruizok@gmail.com>
2022-05-19 11:36:51 +0100

    Add content

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,19 @@ import useGtag from "~hooks/useGtag"
 import { globalStyles } from "~stitches.config"
 import "~styles/globals.css"
 
+const themes = ["light", "dim", "dark"]
+
 function App({ Component, pageProps }: AppProps) {
   globalStyles()
   useGtag()
 
   return (
-    <ThemeProvider>
+    <ThemeProvider
+      storageKey="ruiz_blog_theme"
+      defaultTheme="light"
+      enableSystem
+      themes={themes}
+    >
       <Component {...pageProps} />
     </ThemeProvider>
   )
